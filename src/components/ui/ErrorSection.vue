95ed4f952aa4caf7a8d53ba60b74bfcb90766ab3
Bruno V <bruno_bedretchuk@estudante.sesisenai.org.br>
2022-06-07 17:54:02 -0300

    dist

--- src/components/ui/ErrorSection.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="mx-4 lg:mx-40 2xl:mx-80 coin text-center">
-        <h1 class="text-red-500">Sorry, we could not find the specified coin</h1>
-        <h1 class="text-red-500"><a href="https://api.coingecko.com/api/v3/coins/list" target="_blank">Click here for a full list of all ID's available in the API</a></h1>
-      </div>
-</template>
--- src/components/ui/LoadingSection.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="flex justify-center">
-        <img
-          class="w-1/12 mt-10"
-          src="https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca_w200.gif"
-          alt=""
-        />
-      </div>
-</template>
