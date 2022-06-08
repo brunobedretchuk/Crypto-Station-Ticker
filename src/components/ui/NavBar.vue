8b678d5ccac6bc5eb99ce3c235ca9f1d23ea8cf8
Bruno V <bruno_bedretchuk@estudante.sesisenai.org.br>
2022-06-07 21:42:30 -0300

    navbar debug

--- src/components/ui/NavBar.vue
+++ src/components/ui/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="nav flex gap-2 items-center justify-center mx-2 sm:justify-start ">
+    <nav class="nav flex gap-2 items-center px-2 justify-center sm:justify-start ">
         <span class="flex gap-2"><span><img src="../../assets/logo.png" alt=""></span>
         <span><p class=" text-white font-bold text-xl cStation">Crypto Platform</p></span></span>
 
