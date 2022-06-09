e608ded449ec53e6559b0a13cd8edb4ceb04e403
Bruno V <bruno_bedretchuk@estudante.sesisenai.org.br>
2022-06-09 00:04:40 -0300

    d

--- src/components/views/TheDashboard.vue
+++
@@ -1,9 +0,0 @@
-// this template holds a router-view so inside the '/dashboard' we can access all
-// child routes, each one relative to a specific coin
-// by the default, accessing '/dashboard' redirects directly to the bitcoin subpage
-<template>
- <div>
-  <router-view></router-view>
- </div>
-</template>
-
